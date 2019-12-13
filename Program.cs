8378a33a11c4b5d0c945cce040c7e540d9c44bd7
artem <etomojapo4ta@gmail.com>
2019-12-13 19:21:33 +0200

    sdfsd

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace artzavod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zavod = new Zavod();
+            zavod.Add(new Detal());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Zavod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace artzavod
+{
+    class Zavod
+    {
+        public void Add(Detal detal)
+        {
+
+        }
+    }
+}
